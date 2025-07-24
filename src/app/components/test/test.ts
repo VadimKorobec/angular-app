ba7e50ebb070e21200350a2afe4e0116c33a73e5
VadimKorobec <_s_e_t__@ukr.net>
2025-07-24 15:10:10 +0300

    add some code

--- src/app/components/test/test.ts
+++ src/app/components/test/test.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './test.html',
   styleUrl: './test.scss',
 })
@@ -12,6 +13,9 @@ export class Test {
   lastName = 'Simple';
 
   isEnabled = false;
+
+  isActive = true;
+  isDisabled = false;
 
   getFullName() {
     return `My full name is ${this.firstName} ${this.lastName}`;
