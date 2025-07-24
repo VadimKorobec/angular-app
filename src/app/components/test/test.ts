760295f123bcde52248af3cc73603928855b4a31
VadimKorobec <_s_e_t__@ukr.net>
2025-07-24 14:23:04 +0300

    add some code

--- src/app/components/test/test.ts
+++ src/app/components/test/test.ts
@@ -4,8 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-test',
   imports: [],
   templateUrl: './test.html',
-  styleUrl: './test.scss'
+  styleUrl: './test.scss',
 })
 export class Test {
+  title = 'This is an interpolation.';
+  firstName = 'Cat';
+  lastName = 'Simple';
 
+  isEnabled = false;
+
+  getFullName() {
+    return `My full name is ${this.firstName} ${this.lastName}`;
+  }
 }
